5599b7268b61d9e65e0ba06ef46056663180e065
temito93 <temotemo161@gmail.com>
2022-11-09 23:56:25 +0400

    feat: create login form

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+    <div class="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <img
+                class="mx-auto h-12 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                alt="Your Company"
+            />
+            <h2 class="mt-6 text-center text-3xl font-bold tracking-tight">
+                Log In
+            </h2>
+        </div>
+
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <form class="space-y-6" action="/sessions" method="POST">
+                    @csrf
+
+                    <x-form.input name="email" />
+                    <x-form.input name="password" type="password" />
+
+                    <div>
+                        <button
+                            type="submit"
+                            class="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                        >
+                            Sign in
+                        </button>
+                    </div>
+                </form>
+            </div>
+    </div>
+</x-layout>
