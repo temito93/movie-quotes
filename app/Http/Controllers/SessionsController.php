ea2661a8788eeb48908bf49e6a50a64cda10c51b
temito93 <temotemo161@gmail.com>
2022-11-10 00:06:32 +0400

    feat: create authentication controller

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ValidateUserRequest;
+
 class SessionsController extends Controller
 {
 	//Login Form
 	public function create()
 	{
 		return view('sessions.create');
+	}
+
+	//Login User
+	public function authenticate(ValidateUserRequest $request)
+	{
+		$attributes = $request->validated();
+
+		if (auth()->attempt($attributes))
+		{
+			request()->session()->regenerate();
+
+			return redirect('/admin');
+		}
+
+		return back()->withErrors(['email' => 'Invalid Credentials'])->onlyInput('input');
 	}
 }
