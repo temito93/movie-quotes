ab3fcaef895f8ff6cdebb57e020174e3444e1a94
temito93 <temotemo161@gmail.com>
2022-11-10 00:07:31 +0400

    feat: create route for login form

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 	return view('welcome');
 });
+
+//Show Login Form
+Route::get('/login', [SessionsController::class, 'create'])->middleware('guest');
