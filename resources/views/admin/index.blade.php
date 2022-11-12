800f50e063970e806d64a6b3ae59181561148ccf
temito93 <temotemo161@gmail.com>
2022-11-12 23:31:16 +0400

    feat: add movie list component

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,16 @@
+<x-dashboard>
+    <section>
+        <h2 class="uppercase font-semibold mb-5 text-xl">{{__('admin.movies')}}</h2>
+        @php
+            $movies = App\Models\Movies::latest()->paginate(5);
+        @endphp
+
+           @if($movies->count())
+            <x-show-movie :movies=$movies/>
+             {{$movies->links('pagination::tailwind')}}
+
+            @else
+            <p class="text-center">{{__('admin.noMovies')}} </p>
+            @endif
+    </section>
+</x-dashboard>
