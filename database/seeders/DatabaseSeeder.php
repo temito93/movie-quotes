8d737fa314cf2566b7939db6d6b25253330edf21
temito93 <temotemo161@gmail.com>
2022-11-12 23:16:29 +0400

    feat: add seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,22 +3,18 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Movies;
+use App\Models\Quotes;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-	/**
-	 * Seed the application's database.
-	 *
-	 * @return void
-	 */
 	public function run()
 	{
-		// \App\Models\User::factory(10)->create();
+		$movies = Movies::factory()->create();
 
-		// \App\Models\User::factory()->create([
-		//     'name' => 'Test User',
-		//     'email' => 'test@example.com',
-		// ]);
+		Quotes::factory(5)->create([
+			'movies_id' => $movies->id,
+		]);
 	}
 }
