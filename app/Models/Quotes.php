609b5617a7fad7c1adbfd5090768d97318fe2ceb
temito93 <temotemo161@gmail.com>
2022-11-12 23:45:25 +0400

    feat: add relation with movie in quotes model

--- app/Models/Quotes.php
+++ app/Models/Quotes.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Quotes extends Model
 {
-    use HasFactory;
+	use HasFactory;
+
+	use HasTranslations;
+
+	protected $fillable = ['body', 'image', 'movies_id'];
+
+	public $translatable = ['body'];
+
+	public function movies()
+	{
+		return $this->belongsTo(Movies::class, 'movies_id');
+	}
 }
