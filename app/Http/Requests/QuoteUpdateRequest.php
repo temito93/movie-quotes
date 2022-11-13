b8b480f5fbc91889509ebbd729a60025f14f59d4
temito93 <temotemo161@gmail.com>
2022-11-13 17:47:23 +0400

    chore: remove unused functions from request validations

--- app/Http/Requests/QuoteUpdateRequest.php
+++ app/Http/Requests/QuoteUpdateRequest.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class QuoteUpdateRequest extends FormRequest
 {
-	public function authorize()
-	{
-		return true;
-	}
-
 	public function rules()
 	{
 		return [
--- app/Http/Requests/ValidateQuoteRequest.php
+++ app/Http/Requests/ValidateQuoteRequest.php
@@ -6,11 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ValidateQuoteRequest extends FormRequest
 {
-	public function authorize()
-	{
-		return true;
-	}
-
 	public function rules()
 	{
 		return [
