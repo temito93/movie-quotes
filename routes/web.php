fe6d36126fe96102ec35ad3623a5a29db56cb15a
temito93 <temotemo161@gmail.com>
2022-11-21 14:22:06 +0400

    chore: grouped auth middleware

--- routes/web.php
+++ routes/web.php
@@ -29,23 +29,23 @@ Route::post('/sessions', [SessionsController::class, 'authenticate'])->middlewar
 Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
 //Admin
-Route::prefix('/admin')->middleware('setLocale')->group(function () {
-	Route::get('/main/{locale}', [AdminController::class, 'show'])->middleware('auth')->name('dashboard');
+Route::prefix('/admin')->middleware(['setLocale', 'auth'])->group(function () {
+	Route::get('/main/{locale}', [AdminController::class, 'show'])->name('dashboard');
 	Route::prefix('/{locale}/{movie}')->group(function () {
-		Route::delete('/delete', [MoviesController::class, 'destroy'])->middleware('auth');
-		Route::get('/edit', [MoviesController::class, 'edit'])->middleware('auth')->name('movie_edit');
-		Route::patch('/update', [MoviesController::class, 'update'])->middleware('auth');
+		Route::delete('/delete', [MoviesController::class, 'destroy']);
+		Route::get('/edit', [MoviesController::class, 'edit'])->name('movie_edit');
+		Route::patch('/update', [MoviesController::class, 'update']);
 	});
 	Route::prefix('/{locale}')->group(function () {
-		Route::get('/upload-movie', [MoviesController::class, 'index'])->middleware('auth')->name('movie_upload');
-		Route::post('/movies', [MoviesController::class, 'store'])->middleware('auth');
-		Route::get('/quotes', [QuotesController::class, 'index'])->middleware('auth')->name('show_quotes');
-		Route::get('/upload-quotes', [QuotesController::class, 'show'])->middleware('auth')->name('quote_upload');
-		Route::post('/quotes', [QuotesController::class, 'store'])->middleware('auth');
+		Route::get('/upload-movie', [MoviesController::class, 'index'])->name('movie_upload');
+		Route::post('/movies', [MoviesController::class, 'store']);
+		Route::get('/quotes', [QuotesController::class, 'index'])->name('show_quotes');
+		Route::get('/upload-quotes', [QuotesController::class, 'show'])->name('quote_upload');
+		Route::post('/quotes', [QuotesController::class, 'store']);
 		Route::prefix('/{quote}')->group(function () {
-			Route::get('/edit_quote', [QuotesController::class, 'edit'])->middleware('auth')->name('quote_edit');
-			Route::patch('/update_quote', [QuotesController::class, 'update'])->middleware('auth');
-			Route::delete('/delete_quote', [QuotesController::class, 'destroy'])->middleware('auth');
+			Route::get('/edit_quote', [QuotesController::class, 'edit'])->name('quote_edit');
+			Route::patch('/update_quote', [QuotesController::class, 'update']);
+			Route::delete('/delete_quote', [QuotesController::class, 'destroy']);
 		});
 	});
 });
